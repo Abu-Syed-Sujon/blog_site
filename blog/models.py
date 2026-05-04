256b19a17fd4ccecadb9745b91aa018dc9a9c28f
AbuSyedSujon <sujon.cse.bu@gmail.com>
2026-05-04 16:22:48 +0600

    Post Model Integrated

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    '''Model representing a blog post.'''
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    posted_at = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return str(self.title)
