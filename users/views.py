49cc9ac7ec29d2d9ab98f8717b64bd2fe9a019e0
AbuSyedSujon <sujon.cse.bu@gmail.com>
2026-05-04 21:52:37 +0600

    User Registration by HTML template.

---
+++ users/views.py
@@ -0,0 +1,23 @@
+from django.contrib import messages
+
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+
+def registration_view(request):
+    '''View for user registration.'''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()
+            messages.success(request, f'Account created for {username} successfully!')
+            
+            return redirect('blog-home')  # Redirect to a success page or login page   
+        #else:
+          #  return render(request, 'users/registration.html', {'form': form})
+    else:
+        form = UserCreationForm()
+        
+    return render(request, 'users/registration.html', {'form': form})
